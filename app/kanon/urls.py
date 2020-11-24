b9e6baaf0881a17071203d37ea966919aca907dc
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 11:34:57 +0100

    add index & detail

--- app/kanon/urls.py
+++ app/kanon/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='kanon'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:kanon_id>/', views.detail, name='detail')
 ]
