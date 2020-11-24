80fd934081eeeb666baefb19a8efa7bd81740b50
Joben567 <jobennebur@gmail.com>
2020-11-24 12:27:22 +0100

    Update model

--- app/kanon/models.py
+++ app/kanon/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 class Kanon(models.Model):
     kanon_name = models.CharField(max_length=200)
+    munition = models.ForeignKey('Munition', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.kanon_name
 
 class User(models.Model):
     user_name = models.CharField(max_length=200)
-    kanon = models.ForeignKey(Kanon, on_delete=models.CASCADE)
+    kanon = models.ForeignKey(Kanon, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.user_name
 
 class Munition(models.Model):
     amount = models.IntegerField(default=0)
-    kanon = models.ForeignKey(Kanon, on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.amount)
 
