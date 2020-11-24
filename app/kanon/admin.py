0af19c97d1a25506d9f632ea80f5e096d70cbae5
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 10:45:46 +0100

    init admin

--- app/kanon/admin.py
+++ app/kanon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Kanon,Munition,User
+
+admin.site.register(Kanon)
+admin.site.register(Munition)
+admin.site.register(User)
