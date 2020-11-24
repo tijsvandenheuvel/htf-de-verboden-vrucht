0d8a75acb22b23e63e543c35d75ec4e5908ee09c
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 10:42:44 +0100

    tostring methods

--- app/kanon/models.py
+++ app/kanon/models.py
@@ -3,11 +3,20 @@ from django.db import models
 class User(models.Model):
     user_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.user_name
+
 class Munition(models.Model):
     amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.amount
 
 class Kanon(models.Model):
     kanon_name = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     munition = models.ForeignKey(Munition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.kanon_name
 
