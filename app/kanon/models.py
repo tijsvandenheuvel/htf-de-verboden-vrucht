903f0f669238d4f173377dc6a6de724face19f1b
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 10:28:39 +0100

    create models

--- app/kanon/models.py
+++ app/kanon/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+
+class Munition(models.Model):
+    amount = models.IntegerField(default=0)
+
+class Kanon(models.Model):
+    kanon_name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    munition = models.ForeignKey(Munition, on_delete=models.CASCADE)
+
