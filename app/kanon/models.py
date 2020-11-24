fdd468f3cc7bb630be6122bda31078dafd2bdb96
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 12:01:35 +0100

    fix models

--- app/kanon/models.py
+++ app/kanon/models.py
@@ -1,25 +1,20 @@
 from django.db import models
 
-#test
+class Kanon(models.Model):
+    kanon_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.kanon_name
 
 class User(models.Model):
     user_name = models.CharField(max_length=200)
-
+    kanon = models.ForeignKey(Kanon, on_delete=models.CASCADE)
     def __str__(self):
         return self.user_name
 
 class Munition(models.Model):
-    munition_type = models.CharField(max_length= 50)
     amount = models.IntegerField(default=0)
-
+    kanon = models.ForeignKey(Kanon, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.amount)
-
-class Kanon(models.Model):
-    kanon_name = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    munition = models.ForeignKey(Munition, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.kanon_name
 
