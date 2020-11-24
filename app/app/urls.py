b70c737fd43e4db0785cf058ce44309ae3bcaa48
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2020-11-24 15:48:45 +0100

    fix index url

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from kanon import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('api/', include('kanon.urls')),
     path('admin/', admin.site.urls),
 ]
